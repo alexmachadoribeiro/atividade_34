cc97e4f3267faa531b295057844a44405bb36b50
alexmachadoribeiro <alex.ribeiro@df.docente.senai.br>
2024-09-03 23:49:50 -0300

    Resolvido: lista_09.py

--- lista_09.py
+++ lista_09.py
@@ -1,4 +1,10 @@
+from datetime import date
+
 eventos = []
+
+dia = date.today().day
+mes = date.today().month
+ano = date.today().year
 
 while True:
     opcao = input('1 para cadastrar evento ou 2 para se inscrever: ')
@@ -26,12 +32,20 @@ while True:
                     print('-'*30)
                 try:
                     codigo_evento = int(input('Informe o código do evento: '))
+                    if codigo_evento >= 0 and codigo_evento <= len(eventos):
+                        if idade >= eventos[codigo_evento].get('censura'):
+                            print(f'{nome} foi inscrito no evento. Data da inscrição: {dia}/{mes}/{ano}.')
+                            break
+                        else:
+                            print(f'{nome} não possui idade para se inscrever no evento. Favor escolher outro.')
+                            continue
+                    else:
+                        print('Código do evento inválido.')
+                        continue
                 except Exception as e:
                     print(f'Não foi possível se inscrever para o evento. {e}.')
-                finally:
                     break
-            continue
+            break
         case _:
             print(f'Opção inválida.')
             break
-            # TODO
